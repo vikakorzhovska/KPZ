cf410e82a05318300ee4d78b92e6bc86962821c2
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-02-24 10:42:11 +0200

    Created class Money

---
+++ Lab1/Lab1/Money.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Money
+    {
+        public int WholePart { get; private set; } 
+        public int Cents { get; private set; } 
+        public string Currency { get; private set; } 
+
+        public Money(int wholePart, int cents, string currency)
+        {
+            WholePart = wholePart;
+            Cents = cents;
+            Currency = currency;
+        }
+
+        public void SetAmount(int whole, int cents)
+        {
+            WholePart = whole;
+            Cents = cents;
+        }
+
+        public override string ToString()
+        {
+            return $"{WholePart}.{Cents:D2} {Currency}";
+        }
+    }
+}
