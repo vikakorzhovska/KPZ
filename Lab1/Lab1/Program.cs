abb8fb1bd75bd9f1a5a20230cb4d0062829bbbeb
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-02-24 16:38:45 +0200

    Created class Program

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab1;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        Money price1 = new Money(7, 50, "UAH");
+        Product apple = new Product("Яблуко", "кг", price1, 50, DateTime.Now);
+
+        Money price2 = new Money(15, 00, "UAH");
+        Product potato = new Product("Картопля", "кг", price2, 25, DateTime.Now);
+
+        Warehouse warehouse = new Warehouse();
+        warehouse.AddProduct(apple);
+        warehouse.AddProduct(potato);
+
+        Reporting report = new Reporting();
+
+        Console.WriteLine("Початковий стан складу:");
+        report.InventoryReport(warehouse);
+
+        apple.ReducePrice(1, 20);
+        potato.ReducePrice(3, 50);
+
+        Console.WriteLine("Після зниження цін:");
+        report.InventoryReport(warehouse);
+
+        report.RegisterOfGoodsReceipt(warehouse, "Яблуко", 50, DateTime.Now);
+        report.RegisterOfShipmentOfGoods(warehouse, "Картопля", 20);
+
+        Console.WriteLine("Після надходження та відвантаження товарів:");
+        report.InventoryReport(warehouse);
+    }
+}
