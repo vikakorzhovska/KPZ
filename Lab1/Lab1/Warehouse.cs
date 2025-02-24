52ec06f319a8c77aba05cde5962386e5211ef2ab
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-02-24 11:02:49 +0200

    Created class Warehouse

--- Lab1/Lab1/Warehouse.cs
+++ Lab1/Lab1/Warehouse.cs
@@ -8,5 +8,28 @@ namespace Lab1
 {
     internal class Warehouse
     {
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void RemoveProduct(string productName, int quantity)
+        {
+            var product = products.Find(p => p.Name == productName);
+            if (product != null && product.Quantity >= quantity)
+            {
+                product = new Product(product.Name, product.Unit, product.Price, product.Quantity - quantity, product.LastDeliveryDate);
+            }
+        }
+
+        public void ListProducts()
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
     }
 }
