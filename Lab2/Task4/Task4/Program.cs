684aaa84df1eb7981f8e6245bda02f9ad731e023
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-03-09 14:55:39 +0200

    Completed task 4 in lab 2

---
+++ Lab2/Task4/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Task4;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Virus virus1 = new Virus("Virus A", 17, 5, "Type 1");
+Virus virus2 = new Virus("Virus B", 13, 7, "Type 2");
+Virus virus3 = new Virus("Virus C", 9, 3, "Type 3");
+
+virus1.AddChild(new Virus("Virus A1", 6, 1, "Type 1"));
+virus2.AddChild(new Virus("Virus B1", 9, 2, "Type 2"));
+virus2.AddChild(new Virus("Virus B2", 4, 2, "Type 2"));
+virus3.AddChild(new Virus("Virus C1", 2, 1, "Type 3"));
+
+Virus clonedVirus = (Virus)virus1.Clone();
+
+Console.WriteLine("Оригінальний вірус 1:");
+virus1.DisplayInfo();
+
+Console.WriteLine("\nСклонований Вірус:");
+clonedVirus.DisplayInfo();
---
+++ Lab2/Task4/Task4/Virus.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Virus : IPrototype
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Age { get; set; }
+        public string Species { get; set; }
+        public List<Virus> Children { get; set; }
+
+        public Virus(string name, int weight, int age, string species)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+            Species = species;
+            Children = new List<Virus>();
+        }
+
+        public IPrototype Clone()
+        {
+            Virus clonedVirus = (Virus)this.MemberwiseClone();
+            clonedVirus.Children = new List<Virus>();
+
+            foreach (var child in this.Children)
+            {
+                clonedVirus.Children.Add((Virus)child.Clone());
+            }
+
+            return clonedVirus;
+        }
+
+        public void AddChild(Virus child)
+        {
+            Children.Add(child);
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Вірус: {Name}, Вид: {Species}, Вага: {Weight}, Вік: {Age}");
+            if (Children.Count > 0)
+            {
+                Console.WriteLine("Діти:");
+                foreach (var child in Children)
+                {
+                    child.DisplayInfo();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Без дітей");
+            }
+        }
+    }
+}
