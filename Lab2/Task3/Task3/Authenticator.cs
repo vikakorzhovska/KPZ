a6daaf7573b90af8bc2900ce85f4264d98aa47fb
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-03-09 14:31:12 +0200

    Completed task 3 in lab 2

---
+++ Lab2/Task3/Task3/Authenticator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Authenticator
+    {
+        private static Authenticator _instance;
+
+        private static readonly object _lock = new object();
+
+        private Authenticator() { }
+
+        public static Authenticator GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Authenticator();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public void Authenticate()
+        {
+            Console.WriteLine("Authentication successful!");
+        }
+    }
+}
---
+++ Lab2/Task3/Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿using Task3;
+
+Authenticator auth1 = Authenticator.GetInstance();
+Authenticator auth2 = Authenticator.GetInstance();
+
+Console.WriteLine(ReferenceEquals(auth1, auth2) ? "Same instance!" : "Different instances!");
+
+auth1.Authenticate();
