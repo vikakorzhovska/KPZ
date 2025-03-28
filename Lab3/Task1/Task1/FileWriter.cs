73aa1bda58aa8e73f1958757ecc0f8e19fba6857
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-03-28 16:25:54 +0200

    Completed task 1 in lab 3

---
+++ Lab3/Task1/Task1/FileWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class FileWriter
+    {
+        private readonly string _filePath;
+
+        public FileWriter(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Write(string message)
+        {
+            File.AppendAllText(_filePath, message);
+        }
+
+        public void WriteLine(string message)
+        {
+            File.AppendAllText(_filePath, message + Environment.NewLine);
+        }
+    }
+}
---
+++ Lab3/Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Task1;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Logger consoleLogger = new Logger();
+consoleLogger.Log("Це інформаційне повідомлення");
+consoleLogger.Error("Це повідомлення про помилку");
+consoleLogger.Warn("Це попередження");
+
+FileWriter fileWriter = new FileWriter("logs.txt");
+FileLoggerAdapter fileLogger = new FileLoggerAdapter(fileWriter);
+
+fileLogger.Log("Інформаційне повідомлення");
+fileLogger.Error("Помилка");
+fileLogger.Warn("Попередження");
+
+Console.WriteLine("Логи записані у файлі logs.txt");
