326529997dab8c38d7a45bb132ee09eed4e23118
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-03-29 15:52:47 +0200

    Completed task 5 in lab 3

---
+++ Lab3/Task5/Task5/LightNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public abstract class LightNode
+    {
+        public abstract string OuterHTML();
+        public abstract string InnerHTML();
+    }
+}
